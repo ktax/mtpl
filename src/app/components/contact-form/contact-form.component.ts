7f388c1750d9966b5e0b616c61bfd5ee37e2e346
Kamil Steuer <kamste00@gmail.com>
2018-11-07 20:14:26 +0100

    test

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -41,7 +41,7 @@ export class ContactFormComponent implements OnInit {
     }
 
     const body = new HttpParams()
-      .set('form-name', 'contact')
+      .set('form-name', '"contact"')
       .append('name', this.form.value.name)
       .append('phone', this.form.value.phone)
       .append('email', this.form.value.email)
@@ -49,7 +49,11 @@ export class ContactFormComponent implements OnInit {
 
     this.http
       .post('/', body.toString(), {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        },
       })
       .subscribe((res) => {
         this.form.reset();
