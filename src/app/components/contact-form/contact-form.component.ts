a61085566e3ccd77bfaf12fc6f0f6342ec2ea227
Kamil Steuer <kamste00@gmail.com>
2018-11-09 19:56:17 +0100

    Test

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -41,11 +41,11 @@ export class ContactFormComponent implements OnInit {
     }
 
     const body = new HttpParams()
-      .set('form-name', '"contact"')
-      .append('name', '"' + this.form.value.name + '"')
-      .append('phone', '"' + this.form.value.phone + '"')
-      .append('email', '"' + this.form.value.email + '"')
-      .append('message', '"' + this.form.value.message + '"');
+      .set('form-name', 'contact')
+      .append('name', this.form.value.name)
+      .append('phone', this.form.value.phone)
+      .append('email', this.form.value.email)
+      .append('message', this.form.value.message);
 
     this.http
       .post('/', body.toString(), {
